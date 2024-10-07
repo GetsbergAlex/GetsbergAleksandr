7fecc5908ddf1c55533c0e8275838e3ec586c7a6
GetsbergAlex <aleksander060585@gmail.com>
2024-10-07 15:55:05 +0300

    Add files via upload

---
+++ retuenArrays.java
@@ -0,0 +1,15 @@
+public class retuenArrays {
+    public static void main(String args[]) {
+    }
+    public static int[] returnArrey(int len, int initialValue) {// написать метод принимающий
+        // на вход два аргумента: len и initialValue, и возвращающий одномерный массив типа
+        // int длинной len, каждая ячейка которога равна initialValue.
+        int[] arr2 = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr2[i] = initialValue;
+            System.out.println(arr2[i]);
+
+        }
+        return arr2;
+    }
+}
